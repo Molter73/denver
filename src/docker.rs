2c249a4124d698784d6eb2937573ed749ddbb3b2
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-09-19 18:48:20 +0200

    Simplify run options creation

--- src/docker.rs
+++ src/docker.rs
@@ -1,9 +1,9 @@
 use futures::StreamExt;
 use serde_json::Value;
-use shiplift::{builder::ContainerOptionsBuilder, BuildOptions, ContainerOptions, Docker};
+use shiplift::{BuildOptions, ContainerOptions, Docker};
 
 use crate::cli::{Common, Run};
-use crate::config::{read_config, Config, ContainerConfig, RunConfig};
+use crate::config::{read_config, Config, ContainerConfig};
 
 struct DockerIface {
     docker: Docker,
@@ -53,39 +53,34 @@ impl DockerIface {
         }
     }
 
-    fn handle_run_options(options: &mut ContainerOptionsBuilder, run_options: &RunConfig) {
-        if let Some(args) = &run_options.args {
-            for arg in args {
-                match &arg[..] {
-                    "i" | "interactive" => options.attach_stdin(true),
-                    "rm" => options.auto_remove(true),
-                    _ => options,
-                };
-            }
-        }
-
+    fn create_run_options(name: &str, container: &ContainerConfig) -> ContainerOptions {
+        let run_options = &container.run;
         let workspace_volume = format!("{}:{}", run_options.workspace, run_options.workspace);
         let mut volumes: Vec<&str> = vec![&workspace_volume[..]];
+        let args = run_options.args.as_ref();
 
         if let Some(run_volumes) = &run_options.volumes {
             let mut run_volumes: Vec<&str> = run_volumes.iter().map(|s| &s[..]).collect();
             volumes.append(&mut run_volumes);
         }
 
-        options.volumes(volumes);
-        options.working_dir(&run_options.workspace);
+        ContainerOptions::builder(&container.tag)
+            .name(name)
+            .attach_stdin(
+                args.unwrap_or(&vec![])
+                    .iter()
+                    .any(|e| e == "i" || e == "interactive"),
+            )
+            .auto_remove(args.unwrap_or(&vec![]).iter().any(|e| e == "rm"))
+            .volumes(volumes)
+            .working_dir(&run_options.workspace)
+            .build()
     }
 
-    async fn create_container(&mut self, container: &ContainerConfig) {
+    async fn create_container(&mut self, name: &str, container: &ContainerConfig) {
         let docker = &self.docker;
+        let options = Self::create_run_options(name, container);
 
-        let options = &mut ContainerOptions::builder(container.tag.as_str());
-        let run_options = &container.run;
-
-        Self::handle_run_options(options, run_options);
-
-        options.name("falco-fedora");
-        let options = options.build();
         match docker.containers().create(&options).await {
             Ok(info) => {
                 println!("{}", info.id);
@@ -108,12 +103,13 @@ impl DockerIface {
 pub async fn run(args: &Run) {
     let config = read_config(&args.common.config);
     let mut docker = DockerIface::new(&config);
-    let container = &config.containers[&args.common.container];
+    let name = &args.common.container;
+    let container = &config.containers[name];
 
     if !args.no_rebuild {
         docker.build_image(&args.common, container).await;
     }
-    docker.create_container(container).await;
+    docker.create_container(name, container).await;
     docker.run_container().await;
 }
 
