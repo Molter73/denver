9ef0c166367eab8f20ebe0a703a95c33af23f44f
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-09-20 19:59:25 +0200

    Basic error handling implementation

--- src/denver.rs
+++ src/denver.rs
@@ -1,23 +1,25 @@
-use crate::cli::{Common, Run};
+use crate::cli::{Cli, Commands, Common, Run};
 use crate::config::read_config;
 use crate::docker::DockerClient;
-/*
+
 pub enum DenverError {
     UnknownContainer(String),
 }
-*/
+
 pub struct Denver;
 
 impl Denver {
-    pub async fn run(args: &Run) {
+    pub async fn run(args: &Run) -> Result<(), DenverError> {
         let config = read_config(&args.common.config);
         let mut docker = DockerClient::new(&config);
         let name = &args.common.container;
         let container = &config.containers.get(&args.common.container);
 
         if container.is_none() {
-            eprintln!("Error: {} not found", args.common.container);
-            return;
+            return Err(DenverError::UnknownContainer(format!(
+                "{} not found",
+                args.common.container
+            )));
         }
 
         let container = container.unwrap();
@@ -27,18 +29,36 @@ impl Denver {
         }
         docker.create_container(name, container).await;
         docker.run_container().await;
+
+        Ok(())
     }
 
-    pub async fn build(args: &Common) {
+    pub async fn build(args: &Common) -> Result<(), DenverError> {
         let config = read_config(&args.config);
         let docker = DockerClient::new(&config);
         let container = &config.containers.get(&args.container);
 
         if container.is_none() {
-            eprintln!("Error: {} not found", args.container);
-            return;
+            return Err(DenverError::UnknownContainer(format!(
+                "Error: {} not found",
+                args.container
+            )));
         }
 
         docker.build_image(args, container.unwrap()).await;
+
+        Ok(())
+    }
+}
+
+pub async fn run(cli: Cli) {
+    let result = match cli.command {
+        Commands::Run(args) => Denver::run(&args).await,
+        Commands::Build(args) => Denver::build(&args.common).await,
+    };
+
+    match result {
+        Ok(()) => {}
+        Err(DenverError::UnknownContainer(e)) => eprintln!("Error: {}", e),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
 use clap::Parser;
 mod cli;
-use cli::{Cli, Commands};
+use cli::Cli;
 mod config;
 mod denver;
-use denver::Denver;
 mod docker;
 
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
 
-    match cli.command {
-        Commands::Run(args) => {
-            Denver::run(&args).await;
-        }
-        Commands::Build(args) => {
-            Denver::build(&args.common).await;
-        }
-    }
+    denver::run(cli).await;
 }
